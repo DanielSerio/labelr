1ed9f06f755a35069106568671c32eaab3d95438
DanielSerio <dserio.d@gmail.com>
2021-01-25 14:21:48 -0600

    added simulated get request

--- src/components/partials/Dashboard/Dashboard.tsx
+++ src/components/partials/Dashboard/Dashboard.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { ISpec } from '../../../types';
 import Button from '../../controls/Button/Button';
 import Table from '../../data/Table/Table';
@@ -6,38 +6,49 @@ import AddSpecForm from '../../forms/AddSpecForm/AddSpecForm';
 import { AiOutlineArrowDown, AiOutlineArrowUp } from 'react-icons/ai'
 import './Dashboard.scss';
 import Loading from '../../data/Loading/Loading';
+import { setTimeout } from 'timers';
 
 export default function Dashboard() {
   const [formOpen, setFormOpen] = useState<boolean>(false);
   const toggleForm = () => setFormOpen(!formOpen);
-  const testSpecs: ISpec[] = [
-    {
-      name: 'BS-LS-0002',
-      printMode: 'development'
-    },
-    {
-      name: 'BS-LS-0020',
-      printMode: 'production'
-    },
-    {
-      name: 'BS-LS-0021',
-      printMode: 'production'
-    },
-    {
-      name: 'BS-LS-0022',
-      printMode: 'development'
-    },
-    {
-      name: 'BS-LS-0157',
-      printMode: 'production'
-    },
-  ];
+  const [testSpecs, setTestSpecs] = useState<ISpec[]|null>(null);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setTestSpecs([
+        {
+          name: 'BS-LS-0002',
+          printMode: 'development'
+        },
+        {
+          name: 'BS-LS-0020',
+          printMode: 'production'
+        },
+        {
+          name: 'BS-LS-0021',
+          printMode: 'production'
+        },
+        {
+          name: 'BS-LS-0022',
+          printMode: 'development'
+        },
+        {
+          name: 'BS-LS-0157',
+          printMode: 'production'
+        },
+      ]);
+    }, 1200);
+  }, []);
   return (
     <main className="dashboard">
       <section className="dashboard-container">
         <h1>Specifications</h1>
-        <Table specs={testSpecs}/>
-        {formOpen && <AddSpecForm />}
+
+        {testSpecs && 
+          <Table specs={testSpecs}/>
+        }
+        {(formOpen && testSpecs) && <AddSpecForm />}
+        {!testSpecs && <Loading />}
         <Button type="button" color={formOpen ? 'secondary' : 'primary'} text={formOpen ? 'Close Form' : 'Add a New Spec'} onClick={toggleForm}>
           {!formOpen ? <AiOutlineArrowDown /> : <AiOutlineArrowUp />}
         </Button>
