1d63e104c4a88a9d11e9dd9b3ef8a5f63948d934
DanielSerio <dserio.d@gmail.com>
2021-01-25 14:09:11 -0600

    fixed loading animation

--- src/components/partials/Dashboard/Dashboard.tsx
+++ src/components/partials/Dashboard/Dashboard.tsx
@@ -42,7 +42,6 @@ export default function Dashboard() {
           {!formOpen ? <AiOutlineArrowDown /> : <AiOutlineArrowUp />}
         </Button>
       </section>
-      <Loading />
     </main>
   )
 }
